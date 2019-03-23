3d233a917970dc070132f7cfcaf362186ca8a39a
Adam H. Sparks <adamhsparks@gmail.com>
2019-03-23 20:26:04 +1000

    Create hagis-package.R

---
+++ R/hagis-package.R
@@ -0,0 +1,9 @@
+
+#' Tools for Analysis of _Phytophthora_ Pathotype Complexities, Distributions and Diversity 
+#'
+#' @name hagis
+#' @author Austin McCoy, mccoyaus@@msu.edu and Zachary Noel, noelzach@m@su.edu and
+#'  Adam H. Sparks, adamhsparks@@gmail.com and Martin Chilvers, chilvers@@msu.edu
+#' @docType package
+#'
+"_PACKAGE"
