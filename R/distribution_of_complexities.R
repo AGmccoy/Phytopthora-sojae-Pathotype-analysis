c1bfa0c8eb52fca9aaf1a6b2c9cf6dd4dd1d8d3e
Adam H. Sparks <adamhsparks@gmail.com>
2019-03-29 12:25:08 +1000

    Calculate distribution of complexities

--- R/distribution_of_complexities.R
+++ R/distribution_of_complexities.R
@@ -1,13 +1,12 @@
 
-#' Calculate Distribution of Complexities by Pathogenicity Gene
+#' Calculate Distribution of Complexities by Isolate
 #'
 #' @description This function will calculate the distribution of
-#' susceptibilities by pathogenicity gene.
+#' susceptibilities by isolate.
 #' @inheritParams summarize_rps
 #' @importFrom data.table ":="
 #' @examples
-#' 
-#' 
+#'
 #' # locate system file for import
 #' Ps <- system.file("extdata", "practice_data_set.csv", package = "hagis")
 #'
@@ -23,17 +22,17 @@
 #'                        Rps,
 #'                        perc_susc = "perc.susc")
 #' complexities
-#' 
+#'
 #' @export calc_complexities
 
 calc_complexities = function(x,
-                        cutoff,
-                        control,
-                        sample,
-                        Rps,
-                        perc_susc) {
+                             cutoff,
+                             control,
+                             sample,
+                             Rps,
+                             perc_susc) {
   # CRAN NOTE avoidance
-  Rps <- NULL
+  Rps <- .N <- i.N <- N_samp <- NULL
   
   if (missing(x) |
       missing(cutoff) |
@@ -50,7 +49,7 @@ calc_complexities = function(x,
   # The susceptible control is removed from all isolates in the data set so that
   #  it will not affect complexity calculations and a new data set is made that
   #  it does not contain susceptible controls.
-
+  
   x <- subset(x, Rps != control)
   x[, Rps := droplevels(Rps)]
   
@@ -59,199 +58,113 @@ calc_complexities = function(x,
   x <- .binary_cutoff(.x = x, .cutoff = cutoff)
   
   # set the sample field to factor
-  expr = paste0("x[, ", sample, ":= as.factor(", sample, ")]")
+  expr <- paste0("x[, ", sample, ":= as.factor(", sample, ")]")
   eval(parse(text = expr))
- 
+  
   # Individual isolate complexities as calculated by grouping by "sample" and
   # then summarising the number of "1"s for each "sample" in the "susceptible.1"
   # field, see internal_functions.R
-  y <- .create_summary(.y = x, .Rps = Rps)
-  
-  # left join the summary with original data
-  x[y, on = "Rps", N := i.N]
+  y <- .create_summary_isolate(.y = x, .sample = sample)
   
   # Frequency for each complexity (%).
   # Percent frequency is calculated by taking the individual complexity of each
   # Isolate and grouping all Isolates by their complexity
-  #  (Ind_complexities$Complexity[Ind_complexities$N==X]). The function
-  #   legnth() is then used to identify how many of each complexity there are in
-  #    the data set (from 0 to 13). This number is then taken and divided by the
-  #     total number of isolates in the dataset (which was calculated in
-  #      previous scripts and is identified as "Isolate_n" here) then multiplied
-  #       by 100 for final percentages.
-  # If you are using less than 13 genes, you can edit the script by placing "#"
-  #  before all "Freq_comp_X" that are above your number of genes tested. If
-  #   testing more than 13 genes, you will need to simply copy a line of this
-  #    code and add as many more lines as necessary, making sure to change the
-  #     "N==X" to the correct complexity (i.e. 14, 15, 16, etc)
-  # Changing the script here will require edits on scripts down the line,
-  #  but I will try and annotate where those changes would need to be made, if
-  #   necessary
-  
-  Ind_complexities$Complexity <-
-    as.character(Ind_complexities$N) #adds a column telling the complexity of each individual isolates in the dataset
-  # Determining the frequency of each complexity (0:13) within the data set
-  # 
-  
-  freq_comp_0 <-
-    (length(Ind_complexities$Complexity[Ind_complexities$N == 0]) / Isolate_n) *
-    100
-  freq_comp_1 <-
-    (length(Ind_complexities$Complexity[Ind_complexities$N == 1]) / Isolate_n) *
-    100
-  freq_comp_2 <-
-    (length(Ind_complexities$Complexity[Ind_complexities$N == 2]) / Isolate_n) *
-    100
-  freq_comp_3 <-
-    (length(Ind_complexities$Complexity[Ind_complexities$N == 3]) / Isolate_n) *
-    100
-  freq_comp_4 <-
-    (length(Ind_complexities$Complexity[Ind_complexities$N == 4]) / Isolate_n) *
-    100
-  freq_comp_5 <-
-    (length(Ind_complexities$Complexity[Ind_complexities$N == 5]) / Isolate_n) *
-    100
-  freq_comp_6 <-
-    (length(Ind_complexities$Complexity[Ind_complexities$N == 6]) / Isolate_n) *
-    100
-  freq_comp_7 <-
-    (length(Ind_complexities$Complexity[Ind_complexities$N == 7]) / Isolate_n) *
-    100
-  freq_comp_8 <-
-    (length(Ind_complexities$Complexity[Ind_complexities$N == 8]) / Isolate_n) *
-    100
-  freq_comp_9 <-
-    (length(Ind_complexities$Complexity[Ind_complexities$N == 9]) / Isolate_n) *
-    100
-  freq_comp_10 <-
-    (length(Ind_complexities$Complexity[Ind_complexities$N == 10]) / Isolate_n) *
-    100
-  freq_comp_11 <-
-    (length(Ind_complexities$Complexity[Ind_complexities$N == 11]) / Isolate_n) *
-    100
-  freq_comp_12 <-
-    (length(Ind_complexities$Complexity[Ind_complexities$N == 12]) / Isolate_n) *
-    100
-  freq_comp_13 <-
-    (length(Ind_complexities$Complexity[Ind_complexities$N == 13]) / Isolate_n) *
-    100
-  
-  # Making another dataset which only contains the frequncey of each complexity, for use in data visualization
-  Freq_of_complex <-
-    rbind.x(
-      freq_comp_0,
-      freq_comp_1,
-      freq_comp_2,
-      freq_comp_3,
-      freq_comp_4,
-      freq_comp_5,
-      freq_comp_6,
-      freq_comp_7,
-      freq_comp_8,
-      freq_comp_9,
-      freq_comp_10,
-      freq_comp_11,
-      freq_comp_12,
-      freq_comp_13
-    )
-  colnames(Freq_of_complex) = "Frequency_of_Complexities"
-  # adds another column to the data set so you can easily tell the frequency of each complexity in your data.
-  # This will need to be altered should you be testing a different number of genes. Change the "13" to you maximum complexity possible, without controls
-  Freq_of_complex$complexities <- (0:13)
-  
-  ## Distribution of complexity (counts)
-  # This operates in the same manner as the frequency data, but will only show how many (not percentage) isolates are included in each complexity
-  # Changes made to frequency data for more, or less, genes will also need to be made here.
-  Distr_comp_0 <-
-    length(Ind_complexities$Complexity[Ind_complexities$N == 0])
-  Distr_comp_1 <-
-    length(Ind_complexities$Complexity[Ind_complexities$N == 1])
-  Distr_comp_2 <-
-    length(Ind_complexities$Complexity[Ind_complexities$N == 2])
-  Distr_comp_3 <-
-    length(Ind_complexities$Complexity[Ind_complexities$N == 3])
-  Distr_comp_4 <-
-    length(Ind_complexities$Complexity[Ind_complexities$N == 4])
-  Distr_comp_5 <-
-    length(Ind_complexities$Complexity[Ind_complexities$N == 5])
-  Distr_comp_6 <-
-    length(Ind_complexities$Complexity[Ind_complexities$N == 6])
-  Distr_comp_7 <-
-    length(Ind_complexities$Complexity[Ind_complexities$N == 7])
-  Distr_comp_8 <-
-    length(Ind_complexities$Complexity[Ind_complexities$N == 8])
-  Distr_comp_9 <-
-    length(Ind_complexities$Complexity[Ind_complexities$N == 9])
-  Distr_comp_10 <-
-    length(Ind_complexities$Complexity[Ind_complexities$N == 10])
-  Distr_comp_11 <-
-    length(Ind_complexities$Complexity[Ind_complexities$N == 11])
-  Distr_comp_12 <-
-    length(Ind_complexities$Complexity[Ind_complexities$N == 12])
-  Distr_comp_13 <-
-    length(Ind_complexities$Complexity[Ind_complexities$N == 13])
-  
-  # Producing another dataset for the count/distribution of complexities within the data, same as frequency
-  Distr_of_complex <-
-    rbind.x(
-      Distr_comp_0,
-      Distr_comp_1,
-      Distr_comp_2,
-      Distr_comp_3,
-      Distr_comp_4,
-      Distr_comp_5,
-      Distr_comp_6,
-      Distr_comp_7,
-      Distr_comp_8,
-      Distr_comp_9,
-      Distr_comp_10,
-      Distr_comp_11,
-      Distr_comp_12,
-      Distr_comp_13
-    )
-  colnames(Distr_of_complex) = "Distribution_of_Complexities"
-  # This will need to be altered should you be testing a different number of genes. Change the "13" to you maximum complexity possible, without controls
-  Distr_of_complex$complexities <- (0:13)
-  
-  ## Visualization of Frequency (%)
-  Visualization_of_complexities <-
-    ggplot(data = Freq_of_complex, aes(x = complexities , y = Frequency_of_Complexities)) +
-    geom_bar(stat = "identity") +
-    scale_x_continuous(name = "", breaks = seq(0, 13, 1)) +
-    scale_y_continuous(name = "Percent of Isolates", breaks = seq(0, 60, 2)) +
-    theme_classic() +
-    ggtitle("Percentage of Pathotype Complexities") +
-    my.theme2
-  
-  ## Visualization of Distribution (N per complexity)
-  Visualization_of_Distribution <-
-    ggplot(data = Distr_of_complex, aes(x = complexities , y = Distribution_of_Complexities)) +
-    geom_bar(stat = "identity") +
-    scale_x_continuous(breaks = seq(0, 13, 1)) +
-    scale_y_continuous(breaks = seq(0, 20, 2)) +
-    theme_classic() +
-    ggtitle("Number of Isolates in each Pathotype Complexity") +
-    my.theme2 +
-    ylab(expression(bold("Number of Isolates"))) +
-    xlab("")
-  
-  ## summary statistic (Distribution). Mean, Standard deviation and Standard error of complexities for your data
-  # this will print as 3 = mean, 4 = standard deviation and 5 = standard error
-  comp_mean <- mean(Ind_complexities$N)
-  comp_sd <- sd(Ind_complexities$N)
-  comp_stderr <- std.error(Ind_complexities$N)
   
   
-  # This code will return all of the data which we just made once this chunk has been run, no need to go in and run everything one at a time. Simply run the chunk in an Rmd file and the output will appear either in the console (statistics) or in the plot visualization (frequency and distribution of complexities)
-  final_complexities <- list(
-    FrequencyData = Freq_of_complex,
-    DistributionData = Distr_of_complex,
-    Mean = comp_mean,
-    StandardDev = comp_sd,
-    StandardErr = comp_stderr,
-    FrequencyPlot = Visualization_of_complexities,
-    DistributionPlot = Visualization_of_Distribution
-  )
+  # create an object of the number of genes in the data
+  expr <- paste0("x[, ", Rps, "]")
+  n_gene <- length(unique(eval(parse(text = expr))))
   
-  return(final_complexities)
+  # create an object of the number of samples in the data
+  expr <- paste0("x[, ", sample, "]")
+  n_sample <- length(unique(eval(parse(text = expr))))
+  
+  # create an empty list to populate with frequency values
+  complexities <- vector(mode = "list", length = n_gene)
+  names(complexities) <- seq_along(1:n_gene)
+  
+  for (i in seq_along(1:n_gene)) {
+    complexities[[i]] <- round(
+      length(which(y[, N_samp == i])) / n_sample * 100, 2)
+  }
+  
+  complexities <- data.table::setDT(stack(complexities))
+  names(complexities) <- c("frequency", "N_samp")
+  
+  
+  # distribution of complexity (counts)
+  o <- y[, .N, by = N_samp]
+  o[, N_samp := as.factor(N_samp)]
+  complexities[o, on = "N_samp", distribution := i.N]
+  
+  # set NA to 0 for distribution
+  complexities[is.na(distribution), distribution := 0]
+  data.table::setcolorder(complexities,
+                          neworder = c("N_samp", "frequency", "distribution"))
+  data.table::setnames(complexities, c("sample", "frequency", "distribution"))
+  
+  return(complexities)
+}
+
+
+## Visualization of Frequency (%)
+Visualization_of_complexities <-
+  ggplot(data = Freq_of_complex, aes(x = complexities , y = Frequency_of_Complexities)) +
+  geom_bar(stat = "identity") +
+  scale_x_continuous(name = "", breaks = seq(0, 13, 1)) +
+  scale_y_continuous(name = "Percent of Isolates", breaks = seq(0, 60, 2)) +
+  theme_classic() +
+  ggtitle("Percentage of Pathotype Complexities") +
+  my.theme2
+
+## Visualization of Distribution (N per complexity)
+Visualization_of_Distribution <-
+  ggplot(data = Distr_of_complex, aes(x = complexities , y = Distribution_of_Complexities)) +
+  geom_bar(stat = "identity") +
+  scale_x_continuous(breaks = seq(0, 13, 1)) +
+  scale_y_continuous(breaks = seq(0, 20, 2)) +
+  theme_classic() +
+  ggtitle("Number of Isolates in each Pathotype Complexity") +
+  my.theme2 +
+  ylab(expression(bold("Number of Isolates"))) +
+  xlab("")
+
+## summary statistic (Distribution). Mean, Standard deviation and Standard error of complexities for your data
+# this will print as 3 = mean, 4 = standard deviation and 5 = standard error
+comp_mean <- mean(Ind_complexities$N)
+comp_sd <- sd(Ind_complexities$N)
+comp_stderr <- std.error(Ind_complexities$N)
+
+
+# This code will return all of the data which we just made once this chunk has been run, no need to go in and run everything one at a time. Simply run the chunk in an Rmd file and the output will appear either in the console (statistics) or in the plot visualization (frequency and distribution of complexities)
+final_complexities <- list(
+  FrequencyData = Freq_of_complex,
+  DistributionData = Distr_of_complex,
+  Mean = comp_mean,
+  StandardDev = comp_sd,
+  StandardErr = comp_stderr,
+  FrequencyPlot = Visualization_of_complexities,
+  DistributionPlot = Visualization_of_Distribution
+)
+
+return(final_complexities)
+
+
+
+#' Create Summary Table of Binary Reactions by Sample
+#'
+#' Tally a summary by sample or isolate. This code takes the "Susceptible.1"
+#'  column and summarises it by gene for your total Isolates pathogenic on each
+#'  sample.
+#'
+#' @param .x A `data.table` containing the values to be summarised
+#' @return A `data.table` that tallies the results by sample
+#' @author Adam H. Sparks, adamhsparks@@gmail.com
+#' @noRd
+.create_summary_isolate <- function(.y, .sample) {
+  expr = paste0(".y[, list(N_samp = sum(susceptible.1)), by = list(sample = ",
+                .sample,
+                ")]")
+  y <- eval(parse(text = expr))
+  return(y)
 }
