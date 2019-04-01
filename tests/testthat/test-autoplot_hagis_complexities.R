587efb598a01562788f113629952ea27ad94bf51
adamhsparks <adamhsparks@gmail.com>
2019-04-01 10:38:55 +1000

    Add tests for autoplot methods

---
+++ tests/testthat/test-autoplot_hagis_complexities.R
@@ -0,0 +1,17 @@
+
+# test autoplot.hagis.rps.summary ----------------------------------------------
+context("Test that autoplot.hagis.complexities() works as expected")
+test_that("autoplot.hagis.complexities() returns a ggplot2 object", {
+  Ps <- system.file("extdata", "practice_data_set.csv", package = "hagis")
+  Ps <- read.csv(Ps)
+  complexities <- calculate_complexities(x = Ps,
+                                         cutoff = 60,
+                                         control = "susceptible",
+                                         sample = "Isolate",
+                                         Rps = "Rps",
+                                         perc_susc = "perc.susc")
+  testthat::expect_s3_class(autoplot(complexities,
+                                     type = "frequency"), c("gg", "ggplot"))
+  testthat::expect_s3_class(autoplot(complexities,
+                                     type = "distribution"), c("gg", "ggplot"))
+})
---
+++ tests/testthat/test-autoplot_rps_summary.R
@@ -0,0 +1,14 @@
+
+# test autoplot.hagis.rps.summary ----------------------------------------------
+context("Test that autoplot.hagis.rps.summary() works as expected")
+test_that("autoplot.hagis.rps.summary() returns a ggplot2 object", {
+  Ps <- system.file("extdata", "practice_data_set.csv", package = "hagis")
+  Ps <- read.csv(Ps)
+  rps <- summarize_rps(x = Ps,
+                       cutoff = 60,
+                       control = "susceptible",
+                       sample = "Isolate",
+                       Rps = "Rps",
+                       perc_susc = "perc.susc")
+  testthat::expect_s3_class(autoplot(rps), c("gg", "ggplot"))
+})
