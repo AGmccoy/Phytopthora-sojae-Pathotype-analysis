e86a70426da088347ff8f617c45a3a80d141e6b8
adamhsparks <adamhsparks@gmail.com>
2019-05-24 11:41:34 +1000

    Update my e-mail address to usq

--- R/hagis-package.R
+++ R/hagis-package.R
@@ -3,7 +3,7 @@
 #'
 #' @name hagis
 #' @author Austin McCoy, mccoyaus@@msu.edu and Zachary Noel, noelzach@@msu.edu and
-#'  Adam H. Sparks, adamhsparks@@gmail.com and Martin Chilvers, chilvers@@msu.edu
+#'  Adam H. Sparks, adam.sparks@@usq.edu.au and Martin Chilvers, chilvers@@msu.edu
 #' @docType package
 #'
 "_PACKAGE"
