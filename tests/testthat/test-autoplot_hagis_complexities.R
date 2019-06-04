e9dc67e28b6b1b441449f3818681c14264e9d787
Adam H. Sparks <adamhsparks@gmail.com>
2019-06-05 07:19:23 +1000

    Use "plot" rather than "autoplot" for simplicity

--- tests/testthat/test-autoplot_hagis_complexities.R
+++ tests/testthat/test-autoplot_hagis_complexities.R
@@ -1,7 +1,7 @@
 
-# test autoplot.hagis.gene.summary ----------------------------------------------
-context("autoplot.hagis.complexities()")
-test_that("autoplot.hagis.complexities() returns a ggplot2 object", {
+# test plot.hagis.gene.summary ----------------------------------------------
+context("plot.hagis.complexities()")
+test_that("plot.hagis.complexities() returns a ggplot2 object", {
   Ps <- system.file("extdata", "practice_data_set.csv", package = "hagis")
   Ps <- read.csv(Ps)
   complexities <- calculate_complexities(x = Ps,
@@ -10,21 +10,21 @@ test_that("autoplot.hagis.complexities() returns a ggplot2 object", {
                                          sample = "Isolate",
                                          gene = "Rps",
                                          perc_susc = "perc.susc")
-  complexities_count <- autoplot(complexities, type = "count")
+  complexities_count <- plot(complexities, type = "count")
   expect_doppelganger("Count_complexities",
                               complexities_count)
   
-  complexities_perc <- autoplot(complexities, type = "percentage")
+  complexities_perc <- plot(complexities, type = "percentage")
   expect_doppelganger("Perc_complexities",
                               complexities_perc)
   
-  complexities_perc_blue <- autoplot(complexities, type = "percentage",
+  complexities_perc_blue <- plot(complexities, type = "percentage",
                                      color = "blue")
   expect_doppelganger("Perc_complexities_blue",
                               complexities_perc_blue)
 })
 
-test_that("autoplot.hagis.complexities errors if an invalid type is given", {
-          expect_error(autoplot(complexities, type = "orange"))
+test_that("plot.hagis.complexities errors if an invalid type is given", {
+          expect_error(plot(complexities, type = "orange"))
   })
 
