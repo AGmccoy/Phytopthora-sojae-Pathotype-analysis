a17f4ba81aa5b929d94b94b5cbd5ba8115ebc9ef
Adam H. Sparks <adamhsparks@gmail.com>
2019-06-25 07:13:16 +1000

    Fix Austin's middle initial

--- R/hagis-package.R
+++ R/hagis-package.R
@@ -2,7 +2,7 @@
 #' Tools for Analysis of Plant Pathogen Pathotype Complexities, Distributions and Diversity 
 #'
 #' @name hagis
-#' @author Austin McCoy, mccoyaus@@msu.edu and Zachary Noel, noelzach@@msu.edu and
+#' @author Austin G. McCoy, mccoyaus@@msu.edu and Zachary Noel, noelzach@@msu.edu and
 #'  Adam H. Sparks, adam.sparks@@usq.edu.au and Martin Chilvers, chilvers@@msu.edu
 #' @docType package
 #'
