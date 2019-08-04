364c6f5de89ac61febfb9acb83183ca1c8715f70
Adam Sparks <adamhsparks@gmail.com>
2019-08-05 09:52:12 +1000

    Move data from inst/extdata to /data and start documenting. Austin to finalise

---
+++ R/data.R
@@ -0,0 +1,31 @@
+#' *Phytophthora sojae* Survey Example Data
+#'
+#' Data from a *Phytophthora sojae* survey
+#'
+#' @docType data
+#'
+#' @usage data(P_sojae_survey)
+#'
+#' @format An object of class `data.table` with 294 observations of 12 variables
+#' \describe{
+#'   \item{Isolate}{*P. sojae* isolate identifier}
+#'   \item{Line}{Soybean cultivar}
+#'   \item{Rps}{*Rps* gene identifer}
+#'   \item{Total}{}
+#'   \item{HR (1)}{}
+#'   \item{Lesion (2)}{}
+#'   \item{Lesion to cotyledon (3)}{}
+#'   \item{Dead (4)}{}
+#'   \item{total.susc}{}
+#'   \item{total.resis}{}
+#'   }
+#'
+#' @keywords datasets
+#' 
+#' @source Austin McCoy's experiment to be supplied by Austin here...
+#'
+#' @examples
+#' data(P_sojae_survey)
+#' P_sojae_survey
+#' 
+"P_sojae_survey"
