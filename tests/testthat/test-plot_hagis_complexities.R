aeed1dce2bfeba109892e2b9f2c405cdb93ec60b
Adam H. Sparks <adamhsparks@gmail.com>
2019-08-05 06:18:55 +1000

    Update tests for new graphs and better coverage

--- tests/testthat/test-plot_hagis_complexities.R
+++ tests/testthat/test-plot_hagis_complexities.R
@@ -2,29 +2,46 @@
 # test plot.hagis.gene.summary ----------------------------------------------
 context("plot.hagis.complexities()")
 test_that("plot.hagis.complexities() returns a ggplot2 object", {
-  Ps <- system.file("extdata", "practice_data_set.csv", package = "hagis")
+  Ps <-
+    system.file("extdata", "practice_data_set.csv", package = "hagis")
   Ps <- read.csv(Ps)
-  complexities <- calculate_complexities(x = Ps,
-                                         cutoff = 60,
-                                         control = "susceptible",
-                                         sample = "Isolate",
-                                         gene = "Rps",
-                                         perc_susc = "perc.susc")
+  complexities <- calculate_complexities(
+    x = Ps,
+    cutoff = 60,
+    control = "susceptible",
+    sample = "Isolate",
+    gene = "Rps",
+    perc_susc = "perc.susc"
+  )
   complexities_count <- plot(complexities, type = "count")
-  expect_doppelganger("Count_complexities",
-                              complexities_count)
+  expect_doppelganger("count_complexities",
+                      complexities_count)
   
   complexities_perc <- plot(complexities, type = "percentage")
-  expect_doppelganger("Perc_complexities",
-                              complexities_perc)
+  expect_doppelganger("perc_complexities",
+                      complexities_perc)
   
   complexities_perc_blue <- plot(complexities, type = "percentage",
-                                     color = "blue")
-  expect_doppelganger("Perc_complexities_blue",
-                              complexities_perc_blue)
+                                 color = "blue")
+  expect_doppelganger("perc_complexities_blue",
+                      complexities_perc_blue)
+  
+  complexities_perc_desc_blue <-
+    plot(complexities,
+         type = "percentage",
+         color = "blue",
+         order = "descending")
+  expect_doppelganger("perc_complexities_desc_blue",
+                      complexities_perc_desc_blue)
+  
+  complexities_perc_asc_blue <-
+    plot(complexities,
+         type = "percentage",
+         color = "blue",
+         order = "ascending")
+  expect_doppelganger("perc_complexities_asc_blue",
+                      complexities_perc_asc_blue)
+
+  expect_error(plot(complexities, type = "orange"),
+               regexp = "You have entered an invalid `type`.")
 })
-
-test_that("plot.hagis.complexities errors if an invalid type is given", {
-          expect_error(plot(complexities, type = "orange"))
-  })
-
--- tests/testthat/test-plot_rps_summary.R
+++ tests/testthat/test-plot_rps_summary.R
@@ -2,25 +2,41 @@
 # test plot.hagis.gene.summary ----------------------------------------------
 context("plot.hagis.gene.summary")
 test_that("plot.hagis.gene.summary() returns a ggplot2 object", {
-  Ps <- system.file("extdata", "practice_data_set.csv", package = "hagis")
+  Ps <-
+    system.file("extdata", "practice_data_set.csv", package = "hagis")
   Ps <- read.csv(Ps)
-  rps <- summarize_gene(x = Ps,
-                       cutoff = 60,
-                       control = "susceptible",
-                       sample = "Isolate",
-                       gene = "Rps",
-                       perc_susc = "perc.susc")
-
+  rps <- summarize_gene(
+    x = Ps,
+    cutoff = 60,
+    control = "susceptible",
+    sample = "Isolate",
+    gene = "Rps",
+    perc_susc = "perc.susc"
+  )
+  
   rps_count <- plot(rps, type = "count")
-  expect_doppelganger("Count_summary", rps_count)
+  expect_doppelganger("count_summary", rps_count)
   
   rps_perc <- plot(rps, type = "percentage")
-  expect_doppelganger("Perc_summary", rps_perc)
+  expect_doppelganger("perc_summary", rps_perc)
   
   rps_perc_blue <- plot(rps, type = "percentage", color = "blue")
-  expect_doppelganger("Perc_summary_blue", rps_perc_blue)
-})
+  expect_doppelganger("perc_summary_blue", rps_perc_blue)
+  
+  rps_perc_desc_blue <-
+    plot(rps,
+         type = "percentage",
+         color = "blue",
+         order = "descending")
+  expect_doppelganger("perc_summary_des_blue", rps_perc_desc_blue)
+  
+  rps_perc_asc_blue <-
+    plot(rps,
+         type = "percentage",
+         color = "blue",
+         order = "ascending")
+  expect_doppelganger("perc_summary_asc_blue", rps_perc_asc_blue)
 
-test_that("plot.rps.summary errors if an invalid type is given", {
-  expect_error(plot(rps, type = "orange"))
+  expect_error(plot(rps, type = "orange"),
+               regexp = "You have entered an invalid `type`.")
 })
