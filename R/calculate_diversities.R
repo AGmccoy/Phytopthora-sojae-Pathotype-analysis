f7c31a79b9da7a1595da3e6f727e2a8c6086e6b0
Adam H. Sparks <adamhsparks@gmail.com>
2019-08-06 07:16:25 +1000

    Update documentation and index calculations

--- R/calculate_diversities.R
+++ R/calculate_diversities.R
@@ -2,27 +2,39 @@
 #' Calculate diversities indices
 #'
 #' @description Calculates pathogen diversity indices.
-#' 
+#'
 #' This function calculates five diversity indices for the user.
 #' * Simple diversity index, which will show the proportion of unique pathotypes
 #'  to total samples. As the values gets closer to 1, there is greater diversity
 #'  in pathoypes within the population.
-#'  \deqn{SimpI = \frac{No. pathotypes}{No. samples} }{SimpI = No. pathotypes / No. samples }
+#'  \deqn{S = \frac{No. pathotypes}{No. samples} }{S = No. pathotypes / No. samples }
 #'
 #' * Gleason diversity index, an alternate version of Simple diversity index.
 #'   This index is less sensitive to sample size than the simple index.
-#'   \deqn{GI = \frac{ (No. pathotypes - 1) }{ log( No. samples)}}{GI = (No. pathotypes -1) / log(No. Samples)}
-#'   
-#' * Shannon diversity index index is typically between 1.5 and 3.5. As richness and
-#    evenness of the population increase, so does the Shannon index value
-#' * Simpson Diversity Index, calculated as
-#' * Evenness Diversity Index, calculated as
-#' 
+#'   \deqn{G = \frac{ (No. pathotypes - 1) }{ log( No. samples)}}{G = (No. pathotypes -1) / log(No. Samples)}
+#'
+#' * Shannon diversity index, typically between 1.5 and 3.5. As richness and
+#    evenness of the population increase, so does the Shannon index value.
+#
+#'   \deqn{H' = -\sum_i p_i \log_{b} p_i}{ H = -sum p_i log(b) p_i }, where
+#'   \eqn{p_i} is the proportional abundance of species \eqn{i} and \eqn{b}
+#'   is the base of the logarithm.
+#'
+#' * Simpson diversity index values range from 0 to 1. 1 represents high
+#'    diversity and 0 represents no diversity.
+#'
+#'    \deqn{ D = \sum p_i^2 }{ D = sum p_i^2 }
+#
+#' * Evenness ranges from 0 to 1. As the Eveness value approaches 1, there is a
+#'   more even distribution of each pathoypes frequency within the population.
+#'
+#'   \deqn{E = \frac{Shannon}{log(No. pathotypes) }}{E = Shannon / log(No. Pathotypes) }
+#'
 #' @inheritParams summarize_gene
 #' @examples
 #' # Using the built-in data set, P_sojae_survey
 #' data(P_sojae_survey)
-#' 
+#'
 #' P_sojae_survey
 #'
 #' # calculate susceptibilities with a 60 % cutoff value
@@ -32,9 +44,9 @@
 #'                                      sample = "Isolate",
 #'                                      gene = "Rps",
 #'                                      perc_susc = "perc.susc")
-#' 
+#'
 #' diversities
-#' 
+#'
 #' @export calculate_diversities
 #' @return hagis.diversities object containing
 #'   * Number of Samples
@@ -52,7 +64,6 @@ calculate_diversities <- function(x,
                                   sample,
                                   gene,
                                   perc_susc) {
-  
   # check inptuts and rename fields to work with this package
   x <- .check_inputs(
     .x = x,
@@ -70,15 +81,15 @@ calculate_diversities <- function(x,
   #  it will not affect complexity calculations and a new data set is made that
   #  it does not contain susceptible controls.
   x <- subset(x, gene != control)
-
+  
   # summarise the reactions, create susceptible.1 field, see
   # internal_functions.R
   x <- .binary_cutoff(.x = x, .cutoff = cutoff)
-
+  
   # remove resistant reactions from the data set, leaving only susceptible
   # reactions (pathotype)
   x <- subset(x, susceptible.1 != 0)
-
+  
   # split the data frame by sample and gene
   y <- vapply(split(x[, gene],
                     x[, sample]),
@@ -90,8 +101,8 @@ calculate_diversities <- function(x,
     stringsAsFactors = FALSE
   ))
   
-  table_of_pathotypes <- data.table::as.data.table(
-    table(individual_pathotypes$Pathotype))
+  table_of_pathotypes <-
+    data.table::as.data.table(table(individual_pathotypes$Pathotype))
   data.table::setnames(table_of_pathotypes, c("Pathotype", "Frequency"))
   data.table::setcolorder(table_of_pathotypes, c("Frequency", "Pathotype"))
   
@@ -105,20 +116,20 @@ calculate_diversities <- function(x,
   Simple <- N_pathotypes / N_samples
   Gleason <- (N_pathotypes - 1) / log(N_samples)
   
-  # Shannon diversity index
-  x <- table_of_pathotypes[, Frequency]/(total <- sum(table_of_pathotypes[, Frequency]))
-  x <- -x * log(x, exp(1))
+  # Shannon and Simpson diversity indices
+  x <-
+    table_of_pathotypes[, Frequency] / sum(table_of_pathotypes[, Frequency])
+  
   # Shannon index
-  H <- sum(x, na.rm = TRUE)
+  Shannon <- -x * log(x, exp(1))
+  Shannon <- sum(Shannon, na.rm = TRUE)
   
   # Simpson diversity index
-  x <- table_of_pathotypes[, Frequency]/(total <- sum(table_of_pathotypes[, Frequency]))
-  x <- x*x
+  x <- x * x
   H <- sum(x, na.rm = TRUE)
-  H <- 1 - H
-
-  # Evenness ranges from 0 to 1. As the Eveness value approaches 1, there is a
-  # more even distribution of each pathoypes frequency within the population.
+  Simpson <- 1 - H
+  
+  # Evenness
   Evenness <- Shannon / log(N_pathotypes)
   
   z <-
@@ -165,18 +176,18 @@ print.hagis.diversities <- function(x,
 }
 
 #' Prints Table of Diversities
-#' 
+#'
 #' Print the frequency table of diversities from a `hagis.diversities` object
 #' The resulting object is a [pander] table (a text object for Markdown) for
 #' ease of use in reporting and viewing in the console.
-#' 
+#'
 #' @param x a hagis.diversities object generated by [calculate_diversities()]
 #' @param ... other arguments passed to [pander::panderOptions()]
-#' 
-#' @examples 
+#'
+#' @examples
 #' # Using the built-in data set, P_sojae_survey
 #' data(P_sojae_survey)
-#' 
+#'
 #' P_sojae_survey
 #'
 #' # calculate susceptibilities with a 60 % cutoff value
@@ -186,34 +197,34 @@ print.hagis.diversities <- function(x,
 #'                                      sample = "Isolate",
 #'                                      gene = "Rps",
 #'                                      perc_susc = "perc.susc")
-#' 
+#'
 #' # print the diversities table
 #' diversities_table(diversities)
-#' 
+#'
 #' @return A [pander][pandoc.table] object of diversities
 #' @seealso [calculate_diversities()], [individual_pathotypes()]
 #' @export
-diversities_table <- function(x,...) {
+diversities_table <- function(x, ...) {
   if (class(x)[1] != "hagis.diversities") {
     stop(call. = FALSE,
-         "This is not a hagis.diversities object.") 
+         "This is not a hagis.diversities object.")
   } else {
-      pander::pander(x[[2]], ...)
+    pander::pander(x[[2]], ...)
   }
 }
 
 #' Prints Individual Pathotypes for Each Sample
-#' 
+#'
 #' Print an object from a `hagis.diversities` object with individual pathotypes,
 #' *i.e.* each sample's pathotype. The resulting object is a [pander] table
 #' (a text object for Markdown) for ease of use in reporting and viewing in the
 #' console.
-#' 
+#'
 #' @inheritParams diversities_table
-#' @examples 
+#' @examples
 #' # Using the built-in data set, P_sojae_survey
 #' data(P_sojae_survey)
-#' 
+#'
 #' P_sojae_survey
 #'
 #' # calculate susceptibilities with a 60 % cutoff value
@@ -223,17 +234,17 @@ diversities_table <- function(x,...) {
 #'                                      sample = "Isolate",
 #'                                      gene = "Rps",
 #'                                      perc_susc = "perc.susc")
-#' 
+#'
 #' # print the diversities table
 #' individual_pathotypes(diversities)
-#' 
+#'
 #' @return A [pander][pander] object of individual pathotypes
 #' @seealso [calculate_diversities()], [diversities_table()]
 #' @export
 individual_pathotypes <- function(x, ...) {
   if (class(x)[1] != "hagis.diversities") {
     stop(call. = FALSE,
-         "This is not a hagis.diversities object.") 
+         "This is not a hagis.diversities object.")
   } else
   {
     pander::pander(x[[1]], ...)
