dda47cb226e8b379d35d01b21ad52227b233a8da
Adam Sparks <adamhsparks@gmail.com>
2019-08-14 09:39:30 +1000

    Create defunct fucntions for 'plot' hagis objects

---
+++ R/defunct.R
@@ -0,0 +1,10 @@
+
+plot.hagis.complexities <- function(...) {
+  .Defunct(msg = "'plot()' for hagis complexities objects has been removed\n",
+                 "from this package. Please use 'autoplot() in its place.")
+}
+
+plot.hagis.gene.summary <- function(...) {
+  .Defunct(msg = "'plot()' for hagis complexities objects has been removed\n",
+           "from this package. Please use 'autoplot() in its place.")
+}
