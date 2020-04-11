01b7392b844e2a02616195b550a61ae9070752ba
Adam H. Sparks <adamhsparks@gmail.com>
2020-04-11 16:17:19 +1000

    Remove defunct functions for new release

--- R/defunct.R
+++
@@ -1,9 +0,0 @@
-#' @export
-plot.hagis.complexities <- function(...) {
-  .Defunct(msg = "'plot()' for hagis complexities objects has been removed from this package. Please use 'autoplot() in its place.")
-}
-
-#' @export
-plot.hagis.gene.summary <- function(...) {
-  .Defunct(msg = "'plot()' for hagis complexities objects has been removed from this package. Please use 'autoplot() in its place.")
-}
