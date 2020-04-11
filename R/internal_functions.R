53253e4c46ebbe0db5c1c5f4d4325a34faa32a67
Adam H. Sparks <adamhsparks@gmail.com>
2020-04-11 16:08:47 +1000

    Cleanup and streamline code
    Better documenting, fix data.table returns with DT[] after last :=, import data.table is now called in zzz.R

--- R/internal_functions.R
+++ R/internal_functions.R
@@ -10,40 +10,48 @@
 #' @param .sample column providing the unique identification for each sample
 #'  being tested. Character.
 #' @param .gene column providing the gene(s) being tested. Character.
-#' @param .perc_susc column providing the percent susceptible reactions.
-#' Character.
-#' @import data.table
+#' @param .perc_susc column providing the percent susceptible reactions as a
+#' numeric value. Character.
 #' @noRd
-.check_inputs <- function(.x, .cutoff, .control, .sample, .gene, .perc_susc) {
-  # CRAN NOTE avoidance
-  gene <- sample <- NULL
-  if (!is.data.frame(.x) |
-      !is.numeric(.cutoff) |
-      !is.character(.control) |
-      !is.character(.sample) |
-      !is.character(.gene) |
-      !is.character(.perc_susc)) {
-    stop(call. = FALSE,
-         "You have failed to provide all necessary inputs or\n",
-         "you have provided an improperly formatted item.\n",
-         "Please check and try again.")
+.check_inputs <-
+  function(.x,
+           .cutoff,
+           .control,
+           .sample,
+           .gene,
+           .perc_susc) {
+    # CRAN NOTE avoidance
+    gene <- sample <- NULL
+    if (!is.data.frame(.x) |
+        !is.numeric(.cutoff) |
+        !is.character(.control) |
+        !is.character(.sample) |
+        !is.character(.gene) |
+        !is.character(.perc_susc)) {
+      stop(
+        call. = FALSE,
+        "You have failed to provide all necessary inputs or\n",
+        "you have provided an improperly formatted item.\n",
+        "Please check and try again."
+      )
+    }
+    dt <- as.data.table(.x)
+    setnames(dt,
+             c(.perc_susc, .gene, .sample),
+             c("perc_susc", "gene", "sample"))
+    
+    # validate that perc_susc is numeric
+    if (!is.numeric(dt$perc_susc))
+      stop("Data in the column `perc_susc` must be numeric.")
+    # validate that no values in perc_susc < 0
+    if (any(dt$perc_susc < 0, na.rm = TRUE))
+      stop("Data in the column `perc_susc` must be non-negative.")
+    
+    # set col types for the necessary cols
+    dt[, sample := as.character(sample)]
+    dt[, gene := as.character(gene)]
+    return(dt[])
   }
-  dt <- as.data.table(.x)
-  setnames(dt, c(.perc_susc, .gene, .sample),
-                       c("perc_susc", "gene", "sample"))
-
-  # validate that perc_susc is numeric
-  if (!is.numeric(dt$perc_susc))
-    stop("Data in the column `perc_susc` must be numeric.")
-  # validate that no values in perc_susc < 0
-  if (any(dt$perc_susc < 0, na.rm = TRUE))
-    stop("Data in the column `perc_susc` must be non-negative.")
-
-  # set col types for the necessary cols
-  dt[, sample := as.character(sample)]
-  dt[, gene := as.character(gene)]
-  return(dt)
-}
 
 #' Create binary reaction value
 #'
@@ -52,7 +60,6 @@
 #' @param .x A `data.table` containing the values to be summarised
 #' @param .cutoff Cutoff value for susceptibility
 #' @return A `data.table` that tallies the results by gene
-#' @import data.table
 #' @noRd
 .binary_cutoff <- function(.x, .cutoff) {
   susceptible.1 <- perc_susc <- NULL
@@ -61,5 +68,5 @@
   # used in later analysis.
   .x[, susceptible.1 := 0]
   .x[perc_susc >= .cutoff, susceptible.1 := 1]
-  return(.x)
+  return(.x[])
 }
