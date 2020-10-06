de568fd00fbc21e839d2469d2bcd9f13b47f58fd
Adam H. Sparks <adamhsparks@gmail.com>
2020-10-06 15:46:50 +1000

    Add and document new sample_meta data file for vignette example

--- R/data.R
+++ R/data.R
@@ -35,3 +35,29 @@
 #' P_sojae_survey
 #'
 "P_sojae_survey"
+
+#' *Phytophthora sojae* survey example metadata
+#'
+#' Metadata to accompany internal data from a *Phytophthora sojae* survey used
+#' for an example in the "Beta-diversity Analyses" vignette.
+#'
+#' @docType data
+#'
+#' @usage data(sample_meta)
+#'
+#' @format An object of class `data.frame` with 21 observations of 2 variables
+#' \describe{
+#'   \item{Sample}{*P. sojae* sample identifier}
+#'   \item{Locale}{US State where sample was collected, limited to Michigan}
+#'   }
+#'
+#' @keywords datasets
+#'
+#' @source Data from an ongoing 2017 *Phytophthora sojae* survey in Michigan,
+#' conducted by A. G. McCoy *et al.*.
+#'
+#' @examples
+#' data(sample_meta)
+#' sample_meta
+#'
+"sample_meta"
